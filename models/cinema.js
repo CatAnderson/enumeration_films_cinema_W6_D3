8c438f2771056251d568ac90ab40e6addce1afa6
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-14 14:57:57 +0100

    find titles tested and passing

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,30 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
+Cinema.prototype.filmByTitle = function(){
+    return this.films.map((film) => {
+    return film.title
+  });
+  
+}
+
+// const filmByTitle = (title) => {
+//   return title += title;
+// }
+
+// const result = myNumbers.map(filmByTitle);
+
+
+// // myNumbers.map = functino(multiplyByTwo) {
+//     // const result = []
+//     // for (number of myNumbers){
+//         // doubleNumber = multuplyByTwo
+//         // push doubleNumber to result
+// //     }
+// //  return result
+// // }
+
+
+
 module.exports = Cinema;
